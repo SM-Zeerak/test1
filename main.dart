067c721c9453ba35a4f505a2cfa91d636c99bef7
SM-Zeerak <syed.zeerak67@gmail.com>
2022-03-27 23:21:30 +0500

    Test1

---
+++ main.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+void main(){
+    
+    
+    // //ANS 1
+
+    // int? a = int.parse(stdin.readLineSync()!);
+    // var f = 1;
+
+    // for (var i=a;i>-1;i--){
+    //     f = f*i;
+    //     print(i);
+
+    // }
+    //--------------------------------------------
+
+
+    //Ans2
+   
+    //       int rows = 4;
+    // //Prints upper half pattern
+    //   for (int i = 1; i <= rows; i++) {
+    //     for (int j = 1; j <= i; j++) {
+    //       stdout.write("$j ");
+    //     }
+    //     stdout.writeln();
+    //   }
+    //-----------------------------------------------
+
+    //Ans3
+
+    // var f = 0;
+    // var a = [1,5,9,13,17,21,25,29,33,37];
+
+    // for (var i in a){
+    //     f+=i;
+    // }
+    // print(f);
+    //---------------------------------------------
+
+    //Ans 4
+
+
+    
+
+}
